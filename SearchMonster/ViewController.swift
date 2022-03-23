61de874f629cddb532dfef680b33c8b8497bebba
Y. Murat EKSÜR <ymeksur@yahoo.com>
2022-03-23 18:37:53 +0300

    Initial Commit

---
+++ SearchMonster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SearchMonster
+//
+//  Created by Y. Murat EKSÜR on 23.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
